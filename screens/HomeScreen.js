35ee538ef66a1bdb56e50eea7d296128e480f6db
cataclysmnik <sagniksinghaishere@gmail.com>
2025-02-23 23:54:17 +0530

    Fixed layout on when on-screen keyboard is visible

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -121,8 +121,6 @@ export default function HomeScreen() {
                                         </TouchableOpacity>
                                     );
                                 })
-                                
-                                
                             }
                         </View>
                     ):null
